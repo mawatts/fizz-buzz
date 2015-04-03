46fe8b159ca781e26af39a8f49a2b20f7560e766
mawatts <michaelwatts80@yahoo.com>
2015-04-02 20:52:18 -0400

    Add Alert window when attempting to run app more than once without a reset

--- js/fizz-buzz-app.js
+++ js/fizz-buzz-app.js
@@ -5,27 +5,38 @@ $(document).ready(function() {
 
 function startFizzBuzz() {
 	$('.startButton').click(function() {
-		var x = 0;
-		for (var i = 0; i <= 100; i++) {
-			if (i % 3 == 0 && i % 5 == 0) {
-				x = "Fizz Buzz";
-			}
-			else if (i % 3 == 0) {
-				x = "Fizz";
-			}
-			else if (i % 5 == 0) {
-				x = "Buzz";
-			}
-			else {
-				x = i;
-			}
+		if((document.getElementById("results").value == "") || (document.getElementById("results").value == null)) {
+			/*var x = 0;*/
+			for (var i = 0; i <= 100; i++) {
+				if (i % 3 == 0 && i % 5 == 0) {
+					x = "Fizz Buzz";
+				}
+				else if (i % 3 == 0) {
+					x = "Fizz";
+				}
+				else if (i % 5 == 0) {
+					x = "Buzz";
+				}
+				else {
+					x = i;
+				}
 
-			if (i == 100) {
-				document.getElementById("results").innerHTML = document.getElementById("results").innerHTML + x + ".";
+				if (i == 100) {
+					document.getElementById("results").innerHTML = document.getElementById("results").innerHTML + x + ".";
+				}
+				else {
+					document.getElementById("results").innerHTML = document.getElementById("results").innerHTML + x + ", ";
+				}
 			}
-			else {
-				document.getElementById("results").innerHTML = document.getElementById("results").innerHTML + x + ", ";
-			}
+		}
+		else {
+			swal({
+				title: "Error!",
+				text: "Click the Reset button to clear the Text Area and start over",
+				type: "error",
+				confirmButtonText: "Close",
+				confirmButtonColor: "#FF9100"
+			});		
 		}
 	});
 }
